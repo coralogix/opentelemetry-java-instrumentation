874d54a58da15318713a4f1c43a0135040815997
Rafał Sumisławski <rafal.sumislawski@coralogix.com>
2023-08-25 11:14:41 +0200

    Change to cx version

--- version.gradle.kts
+++ version.gradle.kts
@@ -1,5 +1,5 @@
-val stableVersion = "1.30.0-SNAPSHOT"
-val alphaVersion = "1.30.0-alpha-SNAPSHOT"
+val stableVersion = "1.30.0-cx-SNAPSHOT"
+val alphaVersion = "1.30.0-cx-alpha-SNAPSHOT"
 
 allprojects {
   if (findProperty("otel.stable") != "true") {
