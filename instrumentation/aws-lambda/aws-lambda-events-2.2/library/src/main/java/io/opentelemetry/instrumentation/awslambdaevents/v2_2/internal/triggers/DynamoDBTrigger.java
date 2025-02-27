42a0ffdf508ea2ece93250ad333a72d8e22aa4a0
Israel Blancas <iblancasa@gmail.com>
2025-02-27 11:13:35 +0100

    Add DynamoDB trigger instrumentation for Java. (#13)
    Signed-off-by: Israel Blancas <iblancasa@gmail.com>

---
+++ instrumentation/aws-lambda/aws-lambda-events-2.2/library/src/main/java/io/opentelemetry/instrumentation/awslambdaevents/v2_2/internal/triggers/DynamoDBTrigger.java
@@ -0,0 +1,114 @@
+/*
+ * Copyright The OpenTelemetry Authors
+ * SPDX-License-Identifier: Apache-2.0
+ */
+
+package io.opentelemetry.instrumentation.awslambdaevents.v2_2.internal.triggers;
+
+import static io.opentelemetry.instrumentation.awslambdacore.v1_0.internal.TriggerUtils.FAAS_TRIGGER_TYPE;
+import static io.opentelemetry.instrumentation.awslambdacore.v1_0.internal.TriggerUtils.RPC_REQUEST_PAYLOAD;
+import static io.opentelemetry.instrumentation.awslambdacore.v1_0.internal.TriggerUtils.limitedPayload;
+import static io.opentelemetry.instrumentation.awslambdacore.v1_0.internal.TriggerUtils.logException;
+import static io.opentelemetry.semconv.SemanticAttributes.FAAS_TRIGGER;
+
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent;
+import com.amazonaws.services.lambda.runtime.events.DynamodbEvent.DynamodbStreamRecord;
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import io.opentelemetry.api.common.AttributesBuilder;
+import io.opentelemetry.api.trace.StatusCode;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanKindExtractor;
+import io.opentelemetry.instrumentation.api.instrumenter.SpanStatusBuilder;
+import io.opentelemetry.instrumentation.awslambdacore.v1_0.AwsLambdaRequest;
+import io.opentelemetry.instrumentation.awslambdacore.v1_0.internal.Trigger;
+import io.opentelemetry.instrumentation.awslambdacore.v1_0.internal.TriggerMismatchException;
+import io.opentelemetry.semconv.SemanticAttributes;
+import java.util.List;
+import javax.annotation.Nullable;
+
+/**
+ * This class is internal and is hence not for public use. Its APIs are unstable and can change at
+ * any time.
+ */
+public final class DynamoDBTrigger extends Trigger {
+
+  @Override
+  public boolean matches(AwsLambdaRequest request) {
+    return request.getInput() instanceof DynamodbEvent;
+  }
+
+  /**
+   * Note taken from SqsTrigger:
+   * https://github.com/open-telemetry/semantic-conventions/blob/v1.23.0/docs/messaging/messaging-spans.md#span-name
+   * TODO python uses a different name
+   *
+   * <p>This means that the Python SDK handles it in a different way and we need to do the same here
+   * for consistency.
+   */
+  @Override
+  public String extract(AwsLambdaRequest request) {
+    DynamodbEvent req = requireCorrectEventType(request);
+    if (req.getRecords().size() == 1) {
+      DynamodbStreamRecord record = req.getRecords().get(0);
+      return record.getEventName();
+    } else {
+      return "dynamodb multi trigger";
+    }
+  }
+
+  @Override
+  public void extract(
+      SpanStatusBuilder spanStatusBuilder,
+      AwsLambdaRequest request,
+      @Nullable Object response,
+      @Nullable Throwable error) {
+    if (error != null) {
+      spanStatusBuilder.setStatus(StatusCode.ERROR);
+    }
+  }
+
+  @Override
+  public void onStart(
+      AttributesBuilder attributes, Context parentContext, AwsLambdaRequest request) {
+
+    try {
+      ObjectMapper objectMapper = new ObjectMapper();
+
+      DynamodbEvent event = requireCorrectEventType(request);
+
+      attributes.put(FAAS_TRIGGER, SemanticAttributes.FaasTriggerValues.DATASOURCE);
+      attributes.put(FAAS_TRIGGER_TYPE, "DynamoDB");
+      List<DynamodbStreamRecord> records = event.getRecords();
+      // With DynamoDB trigger we always expect the list to contain exactly one record
+      if (records.size() == 1) {
+        DynamodbStreamRecord record = records.get(0);
+        attributes.put(
+            RPC_REQUEST_PAYLOAD, limitedPayload(objectMapper.writeValueAsString(record)));
+      }
+    } catch (RuntimeException | JsonProcessingException e) {
+      logException(e, "DynamoDBTrigger.onStart instrumentation failed");
+    }
+  }
+
+  @Override
+  public void onEnd(
+      AttributesBuilder attributes,
+      Context context,
+      AwsLambdaRequest request,
+      @Nullable Object response,
+      @Nullable Throwable error) {}
+
+  @Override
+  public SpanKindExtractor<AwsLambdaRequest> spanKindExtractor() {
+    return SpanKindExtractor.alwaysServer();
+  }
+
+  private static DynamodbEvent requireCorrectEventType(AwsLambdaRequest request) {
+    if (!(request.getInput() instanceof DynamodbEvent)) {
+      throw new TriggerMismatchException(
+          "Expected DynamodbEvent. Received " + request.getInput().getClass());
+    }
+    return (DynamodbEvent) request.getInput();
+  }
+}
