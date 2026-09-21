9769d6e6565db3f05aef358138f22b782839182d
Israel Blancas <iblancasa@gmail.com>
2026-09-21 17:53:07 +0200

    Java 2.30.0 upgrade (#17)

--- examples/distro/buildSrc/src/main/kotlin/Versions.kt
+++ examples/distro/buildSrc/src/main/kotlin/Versions.kt
@@ -1,8 +1,8 @@
 // this line is managed by .github/scripts/update-sdk-version.sh
-const val opentelemetrySdkVersion = "1.63.0"
+const val opentelemetrySdkVersion = "1.64.0"
 
 // these lines are managed by .github/scripts/update-version.sh
-const val opentelemetryJavaagentVersion = "2.29.0"
-const val opentelemetryJavaagentAlphaVersion = "2.29.0-alpha"
+const val opentelemetryJavaagentVersion = "2.30.0"
+const val opentelemetryJavaagentAlphaVersion = "2.30.0-alpha"
 
 const val autoserviceVersion = "1.1.1"
--- instrumentation/aws-sdk/aws-sdk-2.2/library/build.gradle.kts
+++ instrumentation/aws-sdk/aws-sdk-2.2/library/build.gradle.kts
@@ -36,7 +36,7 @@ dependencies {
 
 testing {
   suites {
-    val testCoreOnly by registering(JvmTestSuite::class) {
+    register<JvmTestSuite>("testCoreOnly") {
       dependencies {
         implementation(project())
         implementation(project(":instrumentation:aws-sdk:aws-sdk-2.2:testing"))
@@ -49,7 +49,7 @@ testing {
       }
     }
 
-    val testLambda by registering(JvmTestSuite::class) {
+    register<JvmTestSuite>("testLambda") {
       dependencies {
         implementation(project())
         implementation(project(":instrumentation:aws-sdk:aws-sdk-2.2:testing"))
@@ -58,7 +58,7 @@ testing {
       }
     }
 
-    val testBedrockRuntime by registering(JvmTestSuite::class) {
+    register<JvmTestSuite>("testBedrockRuntime") {
       dependencies {
         implementation(project())
         implementation(project(":instrumentation:aws-sdk:aws-sdk-2.2:testing"))
@@ -77,14 +77,14 @@ tasks {
     systemProperty("testLatestDeps", otelProps.testLatestDeps)
   }
 
-  val testStableSemconv by registering(Test::class) {
+  val testStableSemconv = register<Test>("testStableSemconv") {
     testClassesDirs = sourceSets.test.get().output.classesDirs
     classpath = sourceSets.test.get().runtimeClasspath
 
     jvmArgs("-Dotel.semconv-stability.opt-in=database")
   }
 
-  val testCoreOnlyStableSemconv by registering(Test::class) {
+  val testCoreOnlyStableSemconv = register<Test>("testCoreOnlyStableSemconv") {
     val testCoreOnlySourceSet = sourceSets["testCoreOnly"]
     testClassesDirs = testCoreOnlySourceSet.output.classesDirs
     classpath = testCoreOnlySourceSet.runtimeClasspath
@@ -92,7 +92,7 @@ tasks {
     jvmArgs("-Dotel.semconv-stability.opt-in=database")
   }
 
-  val testExceptionSignalLogs by registering(Test::class) {
+  val testExceptionSignalLogs = register<Test>("testExceptionSignalLogs") {
     testClassesDirs = sourceSets.test.get().output.classesDirs
     classpath = sourceSets.test.get().runtimeClasspath
 
